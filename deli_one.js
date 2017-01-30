6fc21a7f4378b6d25b860b66ea8b538a85a7ae02
Han Lazarus <han.lazarus@gmail.com>
2017-01-30 13:52:52 -0800

    polished version

--- deli_one.js
+++ deli_one.js
@@ -7,27 +7,23 @@
 //canvas
 var canvas = document.getElementById('canv-one');
 var context = canvas.getContext('2d');
-canvas.width = 600;
-canvas.height = 400;
-context.lineWidth = 10
-
-var canvasAcc = document.getElementById('canv-acc');
-var contextAcc = canvasAcc.getContext('2d');
+canvas.width = 300;
+canvas.height = 300;
 
  //y height (canvas height)
 const w = 150; //grid sq width
 const h = 150; //grid sq height
 
 var rows = 1//canvas.height/w; //number of rows
-var columns = 3//canvas.width/w; //number of columns
+var columns = 5//canvas.width/w; //number of columns
 var j = 1
 
-const KX1 = 0.013; //X axis amplification - multiplier for difference between resting position and pulled point
+const KX1 = 0.013; //X axus amplification - multiplier for difference between resting position and pulled point
 const KX2 = 0.025; //X axis decay
 const KY1 = 0.01; //Y axis amplification - multiplier for difference between resting position and pulled point
 const KY2 = 0.035; //decay
 
-var parts; //particles
+var parts; //particles 
 var colorCycle = 0; //color offset which gets incremented with time
 var mouseX = 0; //mouse x
 var mouseY = 0; //mouse y
@@ -35,15 +31,9 @@ var mouseDown = false; //mouse down flag\
 var displacementMax = 0;
 var colorScale = 1;
 
-var timeStack = []
-var variablesStack = {}
-var timepoints = 200 //
-
 var Part = function() {
   this.x = 0; //x pos
   this.y = 0; //y pos
-  this.xPull = 0;
-  this.yPull = 0;
   this.vx = 0; //velocity x
   this.vy = 0; //velocity y
   this.ind_x = 0; //index x
@@ -58,12 +48,11 @@ Part.prototype.frame = function frame() {
   if (this.ind_x == 0 || this.ind_x == columns - 1) {
     //pin edges for stability
     this.x = this.ind_x * w;
-    this.y = this.ind_y * h;
+    this.y = this.ind_y * h;  
     return;
   }
 
   //off_dx, off_dy = offset distance x, y
-  //distance from resting position
   var off_dx = this.ind_x * w - this.x;
   var off_dy = this.ind_y * h - this.y;
 
@@ -71,67 +60,33 @@ Part.prototype.frame = function frame() {
   this.off_dy = off_dy;
   this.displacement = Math.sqrt(off_dx * off_dx + off_dy * off_dy);
 
-  this.xPull = 0;
-  this.yPull = 0;
+  var ax = 0;
+  var ay = 0;
 
-  this.xPull -= this.x - parts[this.ind_x - 1].x;
-  this.yPull -= this.y - parts[this.ind_x - 1].y;
+  ax -= this.x - parts[this.ind_x - 1].x;
+  ay -= this.y - parts[this.ind_x - 1].y;
 
-  this.xPull -= this.x - parts[this.ind_x + 1].x;
-  this.yPull -= this.y - parts[this.ind_x + 1].y;
-
-
+  ax -= this.x - parts[this.ind_x + 1].x;
+  ay -= this.y - parts[this.ind_x + 1].y;
 
   //amplification * net pull - decaying damping
-  this.vx += KX1 * this.xPull - KX2 * this.vx;
-  this.vy += KY1 * this.yPull - KY2 * this.vy;
-  this.ind_x * w
-  //this.x += this.vx;
+  this.vx += KX1 * ax - KX2 * this.vx;
+  this.vy += KY1 * ay - KY2 * this.vy;
+
+  this.x += this.vx;
   this.y += this.vy;
 
   if (mouseDown) {
-    var dx = 0//this.x - mouseX;
+    var dx = this.x - mouseX;
     var dy = this.y - mouseY;
-    var displacement = Math.sqrt(dx * dx + dy * dy);
-    if (displacement < 100) {
-      displacement = displacement < 10 ? 10 : displacement;
-      //this.x -= dx / displacement * 5;
-      this.y -= dy / displacement * 5;
+    var ɋ = Math.sqrt(dx * dx + dy * dy);
+    if (ɋ < 100) {
+      ɋ = ɋ < 10 ? 10 : ɋ;
+      this.x -= dx / ɋ * 5;
+      this.y -= dy / ɋ * 5;
     }
   }
 };
-
-Part.prototype.displacementStyle = function displacementStyle(){
-  //note: displacement is always positive,
-  //off_dx and off_dy are positive and negative
-
-  //hue is offset by a cycling color, in a 120 deg window normalized by % max displacement (color)
-  var hue = colorCycle + 120 * this.displacement / colorScale;
-
-  var saturation_offset = 40;
-  var saturation = saturation_offset + this.displacement / colorScale;
-  saturation = saturation > 90 ? 90 : saturation;
-  saturation = saturation < 40 ? 40 : saturation;
-  saturation = saturation + "%";
-
-  var lightness_offset = 60;
-  var lightness = lightness_offset + this.off_dy / colorScale;
-  lightness = lightness > 80 ? 80 : lightness;
-  lightness = lightness < 40 ? 40 : lightness;
-  lightness = lightness + "%";
-
-  // var alpha_offset = 0.6;
-  // var alpha = alpha_offset + this.off_dx;
-  // alpha = alpha > 1 ? 1 : alpha;
-  // alpha = alpha < 0.2 ? 0.2 : alpha;
-  alpha = 1;
-
-  //context.strokeStyle = 'hsla(' + hue + ',' + saturation + ', ' + lightness + ', ' + alpha +')';
-  //context.fillStyle = 'hsla(' + hue + ',' + context.strokeStyle = 'hsla(' + hue + ',' saturation + ', ' + lightness + ', ' + alpha +')';
-  context.fillStyle = `hsla(${hue % 360}, 80%, 50%, 1)`
-  context.strokeStyle = `hsla(${hue % 360}, 80%, 50%, 1)`
-  context.beginPath();
-}
 
 function initializeArray() {
     parts = []; //particle array
@@ -148,18 +103,34 @@ function initializeArray() {
     }
   }
 
-//draw grid
+//draw grid 
 function draw(i) {
   var p = parts[i];
   // these commented lines are fun to play with -- change drawing patterns
   var pAcross = parts[i+1]; //across row
   //var pDown = parts[i + 1][j]; //down column
-  if(pAcross){
+
   context.moveTo(p.x, p.y);
-  context.lineTo(pAcross.x, pAcross.y);
-  }
+  context.lineTo(pAcross.x, pAcross.y); 
 
   context.arc(p.x, p.y, 20, 0, 2*Math.PI);
+  //context.moveTo(p.x, p.y);
+  //context.lineTo(pDown.x, pDown.y); 
+  // var pUp = parts[i][j - 1];
+  // var pDownRight = parts[i + 1][j + 1];
+  // var pDownLeft = parts[i - 1][j + 1];
+
+  // context.moveTo(p.x, p.y);
+  // //context.lineTo(pUp.x, pUp.y); 
+  // context.moveTo(p.x, p.y);
+  // context.lineTo(pDownRight.x, pDownRight.y); 
+  // context.moveTo(p.x, p.y);
+  // context.lineTo(pDownLeft.x, pDownLeft.y); 
+}
+
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 }
 
 //mouse
@@ -172,47 +143,34 @@ canvas.onmousemove = function MSMV(e) {
   mouseY = e.clientY - rect.top;
 }
 
-controlVariables.forEach(item => variablesStack[item] = [])
+window.onresize = resize();
 
 initializeArray();
-run();
 
-function run() {
-  //wipe canvas
-  context.fillStyle = "hsla(0, 5%, 5%, .1)";
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  context.translate(50,50)
-  colorCycle -= 1;
-  displacementMax = 0;
-  //looping through array of points
-  for (var i = 0; i < columns; i++) {
-      var p = parts[i];
-      context.beginPath();
-      p.frame();
-      p.displacementStyle();
-      draw(i);
-      context.stroke();
-      context.fill();
+window.onload = function() {
+  run();
 
+  function run() {
+    //wipe canvas
+    context.fillStyle = "hsla(0, 5%, 5%, .1)";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    colorCycle -= 1;
+    displacementMax = 0;
+    //looping through array of points
+    for (var i = 0; i < columns - 1; i++) {
+      //for (var j = 1; j < rows - 1; j++) {
+        var p = parts[i];
+        context.beginPath();
+        p.frame();
+        p.displacementStyle();
+        draw(i);
+        context.stroke();
+        //displacementMax < p.displacement && p.displacement = displacementMax;
+        //displacementMax = displacementMax > p.displacement ? displacementMax : p.displacement 
+      //}
+      //colorScale = displacementMax;
       colorScale = 100;
-
-    // plot each control variable
-    if(i === 1){
-      // x axis values
-      timeStack.push(Date.now())
-      // y axis values
-      controlVariables.forEach(value => variablesStack[value].push(p[value]))
-      // slide the time window
-      if(timeStack.length > timepoints) {
-        timeStack.shift()
-        controlVariables.forEach(value => variablesStack[value].shift())
-      }
-
-      // live time-based plot
-      controlVariables.forEach(drawCanvas)
     }
+    window.requestAnimationFrame(run);
   }
-
-  window.requestAnimationFrame(run);
-  context.translate(-50,-50)
-}
+};
--- pull.js
+++ pull.js
@@ -1,17 +1,18 @@
-// (function pull(){
   'use strict'
 
   // canvas setup
   var canvas = document.getElementById('pull')
-  canvas.height = 600
-  canvas.width = 600
+  canvas.height = 500
+  canvas.width = 500
   var context = canvas.getContext('2d')
   context.lineWidth = 5
   const radius = 25
-  var hue = 0;
+  var hue = 0
   context.strokeStyle = 'white'
   var drift = 0
-  var driftDirection = 1
+  var driftFlag = false
+  var timeStep = 100
+  var lastAnimationFrameTime = Date.now()
 
   // dom sections
   var div = document.getElementById('pull-section')
@@ -22,17 +23,19 @@
   var xPullLabel = xDiv.getElementsByTagName('label')[0]
   var yPullLabel = yDiv.getElementsByTagName('label')[0]
 
-  xPullValue.value = 0
-  xPullValue.id = 'xPull'
-  xPullLabel.htmlFor = 'xPull'
-  xPullLabel.innerHTML = 'xPull: '
-  // xPullLabel.className = 'flexitem'
+  var driftButton = document.getElementById('drift_button')
 
-  yPullValue.value = 0
-  yPullValue.id = 'yPull'
-  yPullLabel.htmlFor = 'yPull'
-  yPullLabel.innerHTML = 'yPull: '
-  // yPullLabel.className = 'flexitem'
+//   xPullValue.value = 0
+//   xPullValue.id = 'xPull'
+//   xPullLabel.htmlFor = 'xPull'
+//   xPullLabel.innerHTML = 'xPull: '
+//     // xPullLabel.className = 'flexitem'
+
+//   yPullValue.value = 0
+//   yPullValue.id = 'yPull'
+//   yPullLabel.htmlFor = 'yPull'
+//   yPullLabel.innerHTML = 'yPull: '
+//     // yPullLabel.className = 'flexitem'
 
   // div.appendChild(xDiv)
   // xDiv.appendChild(xPullLabel)
@@ -51,55 +54,58 @@
   canvas.onmousedown = () => mouse.down = true
   canvas.onmouseup = () => mouse.down = false
   canvas.onmousemove = mouseMovement
+  driftButton.onmouseup = () => driftFlag = !driftFlag
 
-  function mouseMovement(e){
-      if (!mouse.down){
-          return;
-      }
-      mouse.x = Math.floor(e.x - canvas.getBoundingClientRect().left);
-      mouse.y = Math.floor(e.y - canvas.getBoundingClientRect().top);
+  function mouseMovement(e) {
+    if (!mouse.down) {
+      return;
+    }
+    mouse.x = Math.floor(e.x - canvas.getBoundingClientRect().left);
+    mouse.y = Math.floor(e.y - canvas.getBoundingClientRect().top);
 
-      neighbors.forEach(checkMouse)
+    neighbors.forEach(checkMouse)
   }
 
-  function checkMouse(p){
-      if(mouse.down){
-          let dx = mouse.x - p.x;
-          let dy = mouse.y - p.y;
-          let distance = Math.sqrt(dx * dx + dy * dy);
+  function checkMouse(p) {
+    if (mouse.down) {
+      let dx = mouse.x - p.x;
+      let dy = mouse.y - p.y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
 
-          if(distance < radius){
-              p.x = mouse.x;
-              p.y = mouse.y;
-          }
+      if (distance < radius) {
+        p.x = mouse.x;
+        p.y = mouse.y;
       }
     }
-
-  function driftPoint(neighbor) {
-    drift = 1
-    drift *= driftDirection
-    if (drift > 10) { driftDirection = -1}
-    if (drift < -10) { driftDirection = 1}
-    neighbor.y += drift;
   }
 
+
+
   // four neighbor points that 'pull'
-  const xSpacing = canvas.width/4
-  const ySpacing = canvas.height/4
+  const xSpacing = canvas.width / 4
+  const ySpacing = canvas.height / 4
 
   var xPull = null
   var yPull = null
 
-  var Part = function Part(x,y){
+  var Part = function Part(x, y) {
     this.x = x
     this.y = y
   }
-
-  function drawNeighbor(neighbor){
+  
+  var driftPoint = function driftPoint(neighbor) {
+    var yDrift = 5 * Math.sin(drift)
+    var xDrift = 3 * Math.sin(drift)
+    neighbor.y -= yDrift;
+    neighbor.x += xDrift;
+    drift = (drift + 0.1)
+  }
+  
+  function drawNeighbor(neighbor) {
     context.fillStyle = `hsla(${hue},100%,50%,1)`;
     context.strokeStyle = `hsla(${hue},100%,50%,1)`;
     context.beginPath()
-    context.arc(neighbor.x, neighbor.y, radius, 0, Math.PI*2)
+    context.arc(neighbor.x, neighbor.y, radius, 0, Math.PI * 2)
     context.fill()
     context.moveTo(neighbor.x, neighbor.y)
     context.lineTo(pulledPart.x, pulledPart.y)
@@ -109,14 +115,13 @@
 
   var neighbors = []
   neighbors.push(
-    new Part(3 * xSpacing, 2 * ySpacing), // right
-    new Part(2 * xSpacing, ySpacing), //up
-    new Part(xSpacing, 2 * ySpacing), // left
-    new Part(2 * xSpacing, 3 * ySpacing) // down
-  )
-  // center
+      new Part(3 * xSpacing, 2 * ySpacing), // right
+      new Part(2 * xSpacing, ySpacing), //up
+      new Part(xSpacing, 2 * ySpacing), // left
+      new Part(2 * xSpacing, 3 * ySpacing) // down
+    )
+    // center
   var pulledPart = new Part(2 * xSpacing, 2 * ySpacing)
-
 
 
   // ax -= this.x - parts[this.ind_x - 1][this.ind_y].x;
@@ -133,38 +138,47 @@
 
   // animation loop and initial call
   animate()
-  function animate(){
-    hue -= 0.2 % 360;
-    // context.strokeStyle = `hsla(${hue},100%,50%,1)`;
-    // context.fillStyle = `hsla(${hue},100%,50%,1)`;
-    context.fillStyle = 'black'
-    context.fillRect(0, 0, canvas.width, canvas.height)
-    // driftPoint(neighbors[0])
-    neighbors.forEach(drawNeighbor)
-    context.fillStyle = 'white'
-    context.strokeStyle = 'white'
-    context.beginPath()
-    context.arc(pulledPart.x, pulledPart.y, radius, 0, Math.PI*2)
-    context.fill()
-    context.stroke()
 
-    // calculate pull
-    xPull = neighbors[0].x + neighbors[1].x + neighbors[2].x + neighbors[3].x - pulledPart.x * 4
-    yPull = neighbors[0].y + neighbors[1].y + neighbors[2].y + neighbors[3].y - pulledPart.y * 4
+  function animate() {
+    if(Date.now() > lastAnimationFrameTime + timeStep){    
+      
+      hue -= 0.2 % 360;
+      
+      // context.strokeStyle = `hsla(${hue},100%,50%,1)`;
+      // context.fillStyle = `hsla(${hue},100%,50%,1)`;
+      context.fillStyle = 'black'
+      context.fillRect(0, 0, canvas.width, canvas.height)
+      if(driftFlag){
+        driftPoint(neighbors[0])
+      }
+      // neighbors[0].driftPoint()
+      neighbors.forEach(drawNeighbor)
+      context.fillStyle = 'white'
+      context.strokeStyle = 'white'
+      context.beginPath()
+      context.arc(pulledPart.x, pulledPart.y, radius, 0, Math.PI * 2)
+      context.fill()
+      context.stroke()
 
-    xPullValue.value = xPull
-    yPullValue.value = yPull
+      // calculate pull
+      xPull = neighbors[0].x + neighbors[1].x + neighbors[2].x + neighbors[3].x - pulledPart.x * 4
+      yPull = neighbors[0].y + neighbors[1].y + neighbors[2].y + neighbors[3].y - pulledPart.y * 4
 
-    // draw result vector
-    context.fillStyle = 'red'
-    context.strokeStyle = 'red'
-    context.beginPath()
-    context.moveTo(pulledPart.x, pulledPart.y)
-    context.lineTo(pulledPart.x + xPull, pulledPart.y + yPull)
-    context.lineTo(pulledPart.x + xPull, pulledPart.y)
-    context.lineTo(pulledPart.x, pulledPart.y)
-    context.stroke()
+      xPullValue.value = Math.round(xPull)
+      yPullValue.value = Math.round(yPull)
+
+      // draw result vector
+      context.fillStyle = 'red'
+      context.strokeStyle = 'red'
+      context.beginPath()
+      context.moveTo(pulledPart.x, pulledPart.y)
+      context.lineTo(pulledPart.x + xPull, pulledPart.y + yPull)
+      context.lineTo(pulledPart.x + xPull, pulledPart.y)
+      context.lineTo(pulledPart.x, pulledPart.y)
+      context.stroke()
+
+      lastAnimationFrameTime = Date.now()
+    }
 
     window.requestAnimationFrame(animate)
   }
-// })
